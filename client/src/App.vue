584d5194f62473efe23e0c7eaa7233cd28fee88e
Denisse Abreu <denisseabreu21@yahoo.com>
2021-03-02 13:56:30 -0400

    cambio de navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,45 +1,16 @@
 <template>
   <div id="app">
-      <b-navbar id="fixed" variant="light" class="shadow fixed-top " type="light">
-        <b-navbar-nav>
-          <b-nav-item href="/">{{ $t('home')}}</b-nav-item>
-        <!-- logo -->
-        </b-navbar-nav>
-            <b-navbar-brand class="mx-auto" href="/">
-              <img alt="Vue logo" src="./assets/logo.png">
-            </b-navbar-brand>
-        <!-- dropdown -->
-        <b-navbar-nav>
-          <b-nav-item-dropdown :text="$t('lang')" right>
-          <b-dropdown-item v-for="entry in languages" :key="entry.title" @click="changeLocale(entry.language)">{{entry.title}}</b-dropdown-item>
-        </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-navbar>
-    <AppForm msg="Welcome" :lang="$t('calendar')"/>
+    <nav-bar/>
   </div>
 </template>
 
 <script>
-import AppForm from './components/AppForm.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    AppForm
-  },
-
-  data () {
-    return {
-      languages: [
-        { language: 'en', title: 'English' },
-        { language: 'es', title: 'Español' }
-      ]
-    }
-  },
-  methods: {
-    changeLocale (locale) {
-      this.$i18n.locale = this.$i18n.locale === 'en' ? 'es' : 'en'
-    }
+    NavBar
   }
 }
 </script>
@@ -53,9 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-img {
-  height: 60px;
 }
 </style>
